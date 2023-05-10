4b83f3fda48bb6873d62511ba796f19a655c5346
Md.Rakibul Hasan <125110756+Rakibul-5267@users.noreply.github.com>
2023-05-10 22:30:40 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configuration 
 cloudinary.config({
-  cloud_name: "dvlxatj0a",
-  api_key: "372421991579559",
-  api_secret: "5RvZ6OudQFHTK5W0SOXzGipI7xA"
+  cloud_name: "",
+  api_key: "",
+  api_secret: ""
 });
 
 module.exports = {cloudinary}
